c57da04e783545aa0d45d40f251377610e2a3fae
Marc Cyr <marcacyr@gmail.com>
2013-09-20 14:21:10 -0700

    Update game.rb

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -13,6 +13,11 @@ end
 Given /^I am not yet playing$/ do
 end
 
+Given /^the secret code is "([^"]*)"$/ do |secret|
+  game = Codebreaker::Game.new(output)
+  game.start(secret)
+end
+
 When /^I start a new game$/ do
   game = Codebreaker::Game.new(output)
   game.start

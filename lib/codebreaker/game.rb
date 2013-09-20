fd9fac42dbb37fa1f7347d93e15d8ecc12a836ab
Marc Cyr <marcacyr@gmail.com>
2013-09-20 15:48:48 -0700

    Commit committed

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,31 +1,36 @@
 module Codebreaker
   class Game
     def initialize(output)
-    	@output = output
+      @output = output
     end
+
     def start(secret)
-    	@secret = secret
-    	@output.puts 'Welcome to Codebreaker!'
-    	@output.puts 'Enter guess:'
+      @secret = secret
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
     end
 
     def guess(guess)
-    	if exact_match?(guess, 0)
-    		mark = '+'
-    	elsif number_match?(guess, 0)   		
-    		mark = '-'
-    	else
-    		mark = ''
-    	end
-    	@output.puts mark
+    	mark = ''
+    	(0..3).each do |index|
+    		if exact_match?(guess, index)
+        	mark << '+'
+        end
+      end
+      (0..3).each do |index|
+      	if number_match?(guess, index)
+        	mark << '-'
+        end
+      end
+      @output.puts mark
     end
 
     def exact_match?(guess, index)
-    	guess[index] == @secret[index]
+      guess[index] == @secret[index]
     end
 
     def number_match?(guess, index)
-    	@secret.include?(guess[index])
+      @secret.include?(guess[index])
     end
   end
 end
